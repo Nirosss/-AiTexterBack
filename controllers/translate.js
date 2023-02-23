2461a59485ff064a53f907f1b6d33c88f9b17872
Nirosss <aniroiter@gmail.com>
2023-02-23 21:31:57 +0200

    built setPrompt function

--- controllers/translate.js
+++ controllers/translate.js
@@ -7,10 +7,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 const translate = async (text, to) => {
+  let promptString = setPrompt(text, to)
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `## Rewrite this text:\n${text} in a ${to} English`,
-    temperature: 0.3,
+    prompt: promptString,
+    temperature: 0.7,
     max_tokens: 256,
     top_p: 1.0,
     frequency_penalty: 0.0,
@@ -20,6 +21,28 @@ const translate = async (text, to) => {
   return response.data.choices[0].text
 }
 
+function setPrompt(text, formatStyle) {
+  let prompt = ''
+  switch (formatStyle) {
+    case 'grammar correct':
+      prompt = `## Rewrite this text:\n"${text}"\n in a ${formatStyle} English`
+      break
+    case 'Formal':
+    case 'Casual':
+    case 'Persuasive':
+      prompt = `## Rewrite this text:\n"${text}"\n in a ${formatStyle} English`
+      break
+    case 'Senior program developer':
+    case 'HR specialist':
+      prompt = `## Rewrite this text:\n"${text}"\n like a ${formatStyle} would say it`
+      console.log(prompt)
+      break
+    case 'Corporate':
+      prompt = `## Rewrite this text:\n"${text}"\n in a ${formatStyle} style`
+      break
+  }
+  return prompt
+}
 module.exports = {
   translate,
 }
