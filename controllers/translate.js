318de88bc4a86fee58e26c2d59d5d518815de532
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2023-02-24 10:36:45 +0200

    add prompts

--- controllers/translate.js
+++ controllers/translate.js
@@ -7,9 +7,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 const translate = async (text, to) => {
+  const prompt = getPrompt(text, to)
+  
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `## Rewrite this text:\n${text} in a ${to} English`,
+    prompt,
     temperature: 0.3,
     max_tokens: 256,
     top_p: 1.0,
@@ -18,6 +20,25 @@ const translate = async (text, to) => {
   })
 
   return response.data.choices[0].text
+}
+
+function getPrompt(text, to) {
+  switch (to) {
+    case "Formal":
+      return `Convert the following text into a more formal and official style, giving it a more professional tone:\n\n${text}`
+    case "grammer correct":
+      return `Correct the grammar and spelling of the following text. don't leave any notes about the correction:\n\n${text}`
+    case "Casual":
+      return `Convert the following text into a more casual and popular style, giving it a more natural tone:\n\n${text}`
+    case "Persuasive":
+      return `Make the following text more persuasive, adding a sense of confidence and demand:\n\n${text}`
+    case "Corporate":
+      return `Give the following text a corporate tone by converting it into a formal and official style:\n\n${text}`
+    case "Viral":
+      return `Convert the following text to be a viral social media post:\n\n${text}`
+    default:
+      return text
+  }
 }
 
 module.exports = {
--- routes/translate.js
+++ routes/translate.js
@@ -4,8 +4,8 @@ const { translate } = require('../controllers/translate')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    const {text, from, to} = req.body
-    const translation = await translate(text, from, to)
+    const {text, to} = req.body
+    const translation = await translate(text, to)
     res.json({translation})
 })
 
