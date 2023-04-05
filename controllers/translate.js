9e3b37cb93b0ea6c975162c0bd50f92e52c1af9a
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2023-04-05 11:56:16 +0300

    v2

--- controllers/translate.js
+++ controllers/translate.js
@@ -22,21 +22,22 @@ const translate = async (text, to) => {
     presence_penalty: 0.0,
   })
 
+  return extractSentences(response.data.choices[0].text)
   return response.data.choices[0].text
 }
 
 function getPrompt(text, to) {
   switch (to) {
     case "Formal":
-      return `Convert the following text into a more formal and official style, giving it a more professional tone:\n\n\"${text}\"`
+      return `Display 10 variations of the following text: \n\n\"${text}" \n\n but Convert it into a more formal and official style, giving it a more professional tone`
     case "grammar":
       return `Correct the grammar and spelling of the following text. don't leave any notes about the correction:\n\n\"${text}\"`
     case "Casual":
-      return `Convert the following text into a more casual and popular style, giving it a more natural tone:\n\n\"${text}\"`
+      return `Display 10 variations of the following text: \n\n\"${text}" \n\n but Convert it into a more casual and popular style, giving it a more natural tone`
     case "Persuasive":
-      return `Make the following text more persuasive, adding a sense of confidence and demand:\n\n\"${text}\"`
+      return `Display 10 variations of the following text: \n\n\"${text}" \n\n but Make it to more persuasive, adding a sense of confidence and demand`
     case "Corporate":
-      return `Give the following text a corporate tone by converting it into a formal and official style:\n\n\"${text}"`
+      return `Display 10 variations of the following text: \n\n\"${text}" \n\n but Give it a corporate tone by converting it into a formal and official style"`
     case "Viral":
       return `Convert the following text to be a viral social media post:\n\n\"${text}\"`
     case "Teacher":
@@ -45,9 +46,18 @@ function getPrompt(text, to) {
         return `Convert the following text so it will sound like a spoken in the 60\'s: \n\n\"${text}\"`
       case "Interviewer":
         return `Convert the following text so it will sound like an interviewer with a critical tone: \n\n\"${text}\"`
+      case "Costumers":
+        return `Display 10 variations of the following text: \n\n\"${text}" \n\n BUT Convert the text so it will sound like a business chief talking to customers`
+      case "Documentation":
+        return `Display 10 variations of the following text: \n\n\"${text}" \n\n BUT Convert the text so it will sound like a documentation of an api or a web service`
       default:
         return text
   }
+}
+
+function extractSentences(text) {
+  const sentences = text.split("\n").filter(line => line !== "" && !line.startsWith("n"));
+  return sentences.map(str => str.replace(/^\d+\.\s*/, ''));;
 }
 
 module.exports = {
