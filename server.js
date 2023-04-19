ef8d41244cf9cca79fde1b07f33e377a9c1e61d3
Nirosss <aniroiter@gmail.com>
2023-04-19 10:37:38 +0300

    fixed MIME error

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   const corsOptions = {
     origin: [
       'http://127.0.0.1:5173',
-      'http://localhost:5173',
-      'http://localhost:8080',
-      'http://127.0.0.1:8080',
+      'http://localhost:5173'
     ],
     credentials: true,
   }
